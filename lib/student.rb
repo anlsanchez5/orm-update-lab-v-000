f59634f6681e79de5cbbe7016d3ad6a59cb34fa3
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-05 06:14:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,11 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def self.create(name, grade)
+    new_student = self.new(name, grade)
+    new_student.save
+  end
+
   def update
 
   end
