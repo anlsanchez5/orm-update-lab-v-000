6f93483ecb212efc6aa2d0727e510daf4ac63d09
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-05 06:19:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,10 @@ class Student
       SELECT * FROM students WHERE name = ?
       SQL
 
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end
+      
   end
 
   def update
